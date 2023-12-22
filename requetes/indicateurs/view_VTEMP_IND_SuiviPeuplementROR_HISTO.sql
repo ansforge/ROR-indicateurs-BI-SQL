021c762177b0c033e6c17c8597854769d63f4496
LudivineLacour <ludivine.lacour@esante.gouv.fr>
2023-12-22 18:15:20 +0100

    Evolution tables suivi peuplement

--- requetes/indicateurs/view_VTEMP_IND_SuiviPeuplementROR_HISTO.sql
+++ requetes/indicateurs/view_VTEMP_IND_SuiviPeuplementROR_HISTO.sql
@@ -4,10 +4,8 @@ GO
 CREATE OR ALTER VIEW DLAB_002.VTEMP_IND_SuiviPeuplement_HISTO AS
 
 /*
-Contexte : Indicateurs de suivi du peuplement. Cette vue a été créée de maniére temporaire pour récupérer les données historiques dans un seul endroit, 
-elle sera remplacée par la table d'historisation en cours de fiabilisation par Umanis.
-Version : 1.8
-Notes derniére évolution : Suppression des ajoutes manuels sur la région Nouvelle-Aquitaine à la suite de l'enregistrement manuel des statistiques dans la table HISTO
+Description : Vue créée de maniére temporaire pour afficher les indicateurs de peuplement sur le tableau de bord PowerBI. 
+Deux régions sont exclues des indicateurs à date car elles n'ont pas validé l'automatisation de leurs statistiques.
 Sources : 
   - T_IND_SuiviPeuplementROR_HISTO
   - V_IND_SuiviPeuplementROR
@@ -19,9 +17,9 @@ SELECT
 	DT_Reference
 	,Periodicite
 	,CodeRegion
+	,'-2' AS CodeDepartement
 	,ChampActivite
 	,CodeCategorieEG
-	,LibelleCategorieEG
 	,TypePerimetre
 	,SecteurEG
 	,NB_EG_PerimetreFiness
@@ -31,15 +29,15 @@ SELECT
 	,DT_UPDATE_TECH
 FROM DATALAB.DLAB_002.T_IND_SuiviPeuplementROR_HISTO
 -- Récupération uniquement des précédents trimestres. Le trimestre en cours est calculé dans la 2nde partie de la requete.
-WHERE DT_Reference < '2023-10-01'
+WHERE DT_Reference <= CAST(DATEADD(qq, DATEDIFF(qq,0, GETDATE()), 0)-1 AS DATE)
 UNION ALL
 SELECT
-DT_Reference
+	DT_Reference
 	,Periodicite
 	,CodeRegion
+	,CodeDepartement
 	,ChampActivite
 	,ISNULL(CodeCategorieEG,'-1') AS CodeCategorieEG
-	,LibelleCategorieEG
 	,TypePerimetre
 	,SecteurEG
 	,NB_EG_PerimetreFiness
--- requetes/indicateurs/view_V_IND_SuiviPeuplementROR.sql
+++ requetes/indicateurs/view_V_IND_SuiviPeuplementROR.sql
@@ -4,69 +4,34 @@ GO
 CREATE OR ALTER VIEW DLAB_002.V_IND_SuiviPeuplementROR AS
 
 /*
-Contexte : Vue agregee permettant de calculer les indicateurs de suivi du peuplement
-Version de la vue : 1.3
-Notes derniere evolution : Remplacement de la source INIT par la table HISTO + correctif sur le calcul de date utilie dans la jointure avec la table HISTO
+Description : Vue d'agregation permettant de calculer les indicateurs de suivi du peuplement
 Sources : 
   - V_DIM_SuiviPeuplementROR_EG
-  - T_IND_SuiviPeuplementROR_HISTO
 Vue utilisee par :
   - VTEMP_IND_SuiviPeuplementROR_HISTO
-  - Traitement d'historisation sur la table V_IND_SuiviPeuplementROR_HISTO
+  - Traitement d'historisation sur la table T_IND_SuiviPeuplementROR_HISTO
 */
 
-
 SELECT
-	actuel.DT_Reference
-	,actuel.Periodicite
-	,actuel.CodeRegion
-	,actuel.ChampActivite
-	,actuel.CodeCategorieEG
-	,actuel.LibelleCategorieEG
-	,actuel.SecteurEG
-	,actuel.TypePerimetre
-	,actuel.NB_EG_PerimetreFiness
-	,actuel.NB_EG_PeuplementFinalise
-	,actuel.NB_EG_PeuplementEnCours
-	,actuel.NB_EG_PeuplementAFaire
-	-- Si la categorie d'EG n'existait pas dans les chiffres de la periode precedente (aucun etablissement ou perimetre non suivi) alors la valeur de la colonne est NULL
-	,CASE 
-		WHEN histo.NB_EG_PerimetreFiness is null THEN NULL
-		ELSE actuel.NB_EG_PerimetreFiness - histo.NB_EG_PerimetreFiness
-	END AS NB_VariationPerimetreFiness
-	-- Idem condition precedente
-	,CASE 
-		WHEN histo.NB_EG_PeuplementFinalise is null THEN NULL
-		ELSE actuel.NB_EG_PeuplementFinalise - histo.NB_EG_PeuplementFinalise
-	END AS NB_VariationPeuplementFinalise
-FROM (
-	SELECT
-		-- Calcul du trimetre de reference (dernier jour du trimetre) e partir de la date du jour e laquelle la requete tourne
-		CAST(DATEADD(q,1,DATEADD(qq, DATEDIFF(qq,0, GETDATE()), 0))-1 as date) AS DT_Reference
-		,'trimestre' AS Periodicite
-		,CodeRegion
-		,ChampActivite
-		,CodeCategorieEG
-		,LibelleCategorieEG
-		,SecteurEG
-		,TypePerimetre
-		,COUNT(*) AS NB_EG_PerimetreFiness
-		,COUNT(CASE WHEN StatutPeuplement = 'Finalise' THEN NumFINESS_EG END) AS NB_EG_PeuplementFinalise
-		,COUNT(CASE WHEN StatutPeuplement = 'En cours' THEN NumFINESS_EG END) AS NB_EG_PeuplementEnCours
-		,COUNT(CASE WHEN StatutPeuplement = 'A faire' THEN NumFINESS_EG END) AS NB_EG_PeuplementAFaire
-	FROM DATALAB.DLAB_002.V_DIM_SuiviPeuplementROR_EG
-	-- Filtre sur le flag pour recuperer de la table source uniquement le perimetre suivi
-	WHERE FG_PerimetreSuiviPeuplement = 1
-	GROUP BY CodeRegion
+	-- Calcul du trimetre de reference (dernier jour du trimetre) a partir de la date du jour
+	CAST(DATEADD(q,1,DATEADD(qq, DATEDIFF(qq,0, GETDATE()), 0))-1 as date) AS DT_Reference
+	,'trimestre' AS Periodicite
+	,CodeRegion
+	,CodeDepartement
 	,ChampActivite
 	,CodeCategorieEG
-	,LibelleCategorieEG
+	,SecteurEG
 	,TypePerimetre
-	,SecteurEG) AS actuel
--- Jointure avec la table historisee pour calculer les variations entre les periodes
-LEFT JOIN DATALAB.DLAB_002.T_IND_SuiviPeuplementROR_HISTO AS histo
-	ON actuel.CodeRegion = histo.CodeRegion 
-	AND actuel.ChampActivite = histo.ChampActivite
-	AND actuel.CodeCategorieEG = histo.CodeCategorieEG
-	-- Filtre sur le trimestre precedent a celui du jour ou la requete tourne
-	AND histo.DT_Reference = CAST(DATEADD(qq, DATEDIFF(qq,0, GETDATE()), 0)-1 AS DATE)
+	,COUNT(*) AS NB_EG_PerimetreFiness
+	,COUNT(CASE WHEN StatutPeuplement = 'Finalise' THEN NumFINESS_EG END) AS NB_EG_PeuplementFinalise
+	,COUNT(CASE WHEN StatutPeuplement = 'En cours' THEN NumFINESS_EG END) AS NB_EG_PeuplementEnCours
+	,COUNT(CASE WHEN StatutPeuplement = 'A faire' THEN NumFINESS_EG END) AS NB_EG_PeuplementAFaire
+FROM DATALAB.DLAB_002.V_DIM_SuiviPeuplementROR_EG
+-- Filtre sur le flag pour recuperer de la table source uniquement le perimetre suivi
+WHERE FG_PerimetreSuiviPeuplement = 1
+GROUP BY CodeRegion
+,CodeDepartement
+,ChampActivite
+,CodeCategorieEG
+,TypePerimetre
+,SecteurEG

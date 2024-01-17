8e5f912bf08fde07e21bac702bc853a0551c0f5d
LudivineLacour <ludivine.lacour@esante.gouv.fr>
2024-01-17 14:42:24 +0100

    Ajout code SVG vue département

--- requetes/referentiels/view_V_REF_Departement.sql
+++ requetes/referentiels/view_V_REF_Departement.sql
@@ -7,6 +7,7 @@ SELECT
 	,AK_Departement
 	,TX_LibelleDepartement
 	,FK_RegionInsee
+	,TX_CodeSVGDepartement
 FROM COMMUN_DWH.dbo.T_REF_Departement
 INNER JOIN DATALAB.DLAB_002.V_REF_Region
 	ON T_REF_Departement.FK_RegionInsee = V_REF_Region.Id_Region
